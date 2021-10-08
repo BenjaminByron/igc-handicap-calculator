b1e17c50b5c19a9aa33ea18e6ee039a53bd4fc84
Clement Allen <clement.a.allen@accenture.com>
2021-10-08 20:58:10 +0100

    Added calculate function

--- js/fetch.js
+++ js/fetch.js
@@ -9,27 +9,31 @@ dropdown.selectedIndex = 0;
 
 const url = 'https://myjson.dit.upm.es/api/bins/1pan';
 
+let aircraftData;
+
 fetch(url)
-  .then(  
-    function(response) {  
-      if (response.status !== 200) {  
-        console.warn('Looks like there was a problem. Status Code: ' + 
-          response.status);  
-        return;  
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.warn('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
       }
 
-      response.json().then(function(data) {  
+      response.json().then(function(data) {
         let option;
+
+        aircraftData = data;
 
         for (let i = 0; i < data.length; i++) {
             option = document.createElement('option');
               option.text = data[i].type;
               option.value = data[i].type;
               dropdown.add(option);
-          }    
-        });  
-      }  
-    )  
-    .catch(function(err) {  
-      console.error('Fetch Error -', err);  
+          }
+        });
+      }
+    )
+    .catch(function(err) {
+      console.error('Fetch Error -', err);
     });
