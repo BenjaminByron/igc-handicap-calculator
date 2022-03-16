4235e6949503c36e65503f29d6ff01b6f34588a8
Ben Hughes <91264347+BenjaminByron@users.noreply.github.com>
2022-03-16 16:05:23 +0000

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,39 @@
+let dropdown = document.getElementById('glider-dropdown');
+dropdown.length = 0;
+
+let defaultOption = document.createElement('option');
+defaultOption.text = 'Select Your Club Class Weapon';
+
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+
+const url = 'https://api.jsonbin.io/b/62320a9fa703bb67492d6960';
+
+let aircraftData;
+
+fetch(url)
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.warn('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      response.json().then(function(data) {
+        let option;
+
+        aircraftData = data;
+
+        for (let i = 0; i < data.length; i++) {
+            option = document.createElement('option');
+              option.text = data[i].type;
+              option.value = data[i].type;
+              dropdown.add(option);
+          }
+        });
+      }
+    )
+    .catch(function(err) {
+      console.error('Fetch Error -', err);
+    });
