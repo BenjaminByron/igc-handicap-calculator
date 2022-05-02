883edb7f772a4d9e7bc65fca35f8b88b87e4d52a
Ben Hughes <91264347+BenjaminByron@users.noreply.github.com>
2022-05-02 09:34:21 +0100

    More .json fun

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const matchList = document.getElementById('match-list');
 
 // Search handicap_list.json and filter
 const searchGliders = async searchText => {
-  const res = await fetch('../data/handicap_list.json');
+  const res = await fetch('https://api.jsonbin.io/b/62320a9fa703bb67492d6960');
   const gliders = await res.json();
 
 // Get matches to current text input
