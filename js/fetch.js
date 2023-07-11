b9ad7892a2109903e4307b0896f1da1361b2e46d
Ben Hughes <91264347+BenjaminByron@users.noreply.github.com>
2023-07-11 15:05:23 +0200

    Add files via upload

--- js/fetch.js
+++ js/fetch.js
@@ -7,7 +7,7 @@ defaultOption.text = 'Select Your Club Class Weapon';
 dropdown.add(defaultOption);
 dropdown.selectedIndex = 0;
 
-const url = 'https://myjson.dit.upm.es/api/bins/1pan';
+const url = 'https://api.jsonbin.io/v3/b/62320a9fa703bb67492d6960';
 
 let aircraftData;
 
